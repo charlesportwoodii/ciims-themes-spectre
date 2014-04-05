f9dcc5f576802935ea0dcb10782bba6224da4597
Charles R. Portwood II <charlesportwoodii@users.noreply.github.com>
2014-04-05 09:52:01 -0500

    Fixes #1

--- views/site/error.php
+++ views/site/error.php
@@ -1,4 +1,4 @@
 <div class="login-container">
 		<h1 class="content-subhead"><?php echo Yii::t('SpectreTheme', 'Error {{code}}', array('{{code}}' => $error['code'])); ?></h1>
-		<p class="alert"><?php echo $error['message']; ?></p>
+		<p class="alert"><?php echo CHtml::encode($error['message']); ?></p>
 </div>
