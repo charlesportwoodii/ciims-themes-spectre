288b84380ab95b81dbd76f29fc7976a593461ffc
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2014-06-11 19:33:52 -0500

    Starting refactorings for compatability with 2.0.0

--- Theme.php
+++ Theme.php
@@ -1,9 +1,15 @@
 <?php
 
+// Load the local autoloder if it exists
+if (file_exists(__DIR__.DS.'vendor'.DS.'autoload.php'))
+	require __DIR__.DS.'vendor'.DS.'autoload.php';
+
+// Import CiiSettingsModel
+Yii::import('application.modules.dashboard.components.CiiSettingModel');
 class Theme extends CiiThemesModel
 {
-    /**
+	/**
      * @var string  The theme name
      */
-	private $theme = 'spectre';
+	public $theme = 'spectre';
 }
