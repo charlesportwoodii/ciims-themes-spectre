54da3b8ff2fa1de5480b3cb129f10f8715b14570
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2015-03-22 18:28:56 +0000

    Updating registration email template

--- views/email/register.php
+++ views/email/register.php
@@ -1,7 +1,7 @@
 <?php echo $user->displayName; ?>,<br /><br />
 <?php echo Yii::t('themes.spectre.main', 'Thanks for registering your account at CiiMS Blog {{blog}}. To verify your account, {{clickhere}}. This action will verify your email address and allow you to interact with features only available to registered users.', array(
 		'{{blog}}' => CHtml::link(Cii::getConfig('name', Yii::app()->name), Yii::app()->createAbsoluteUrl('/')),
-		'{{clickhere}}' => CHtml::link(Yii::t('themes.spectre.main', 'click here'), Yii::app()->createAbsoluteUrl('/activation/'.$user->id.'/'.$hash))
+		'{{clickhere}}' => CHtml::link(Yii::t('themes.spectre.main', 'click here'), Yii::app()->createAbsoluteUrl('/activation/'.$hash))
 )); ?>
 <br /><br />
 <?php echo Yii::t('themes.spectre.main', 'Thank you.'); ?><br /><br />
