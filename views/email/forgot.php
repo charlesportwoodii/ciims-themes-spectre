86b4cdc763a3c36c655e499296b9a68cc0fd37b8
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2015-03-22 20:07:50 +0000

    Fixing password reset email

--- views/email/forgot.php
+++ views/email/forgot.php
@@ -1,8 +1,8 @@
-<?php echo $user->displayName; ?>,<br /><br />
-<?php echo Yii::t('themes.spectre.main', 'You recently notified us that you forgot your password for the following blog: {{blog}}. To reset your password, {{clickhere}} and follow the instructions on the reset page. This link is valid for 15 minutes.', array(
+<?php echo $user->username; ?>,<br /><br />
+<?php echo Yii::t('themes.default.main', 'You recently notified us that you forgot your password for the following blog: {{blog}}. To reset your password, {{clickhere}} and follow the instructions on the reset page. This link is valid for 15 minutes.', array(
 	'{{blog}}' => CHtml::link(Cii::getConfig('name', Yii::app()->name), Yii::app()->createAbsoluteUrl('/')),
-	'{{clickhere}}' => CHtml::link(Yii::t('themes.spectre.main', 'click here'), Yii::app()->createAbsoluteUrl('/forgot/' . $hash))
+	'{{clickhere}}' => CHtml::link(Yii::t('themes.default.main', 'click here'), Yii::app()->createAbsoluteUrl('/resetpassword/' . $hash))
 )); ?>
 <br /><br />
-<?php echo Yii::t('themes.spectre.main', 'Thank you.'); ?><br /><br />
-<?php echo Yii::t('themes.spectre.main', 'P.S. If you did not request this email, you may safely ignore it.'); ?>
+<?php echo Yii::t('themes.default.main', 'Thank you.'); ?><br /><br />
+<?php echo Yii::t('themes.default.main', 'P.S. If you did not request this email, you may safely ignore it.'); ?>
